17e41c705dffd00b90e3d90434ce493efbb0fc9f
aditya <adityakumar222333@gmail.com>
2022-04-12 03:21:13 +0530

    Finally notes can be added to the note-container with javaScript
    but there are a few downsides
    1. The code is very messed up and needs some refactoring
    2. The code is not very readable
    3. There is no way to delete notes
    4. Notes disappear when the page is refreshed

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,45 @@
 console.log('This is live')
+// const searchBox = document.getElementById('search-box')
+
+const createNoteBtn = document.querySelector('.add-note-btn')
+let parent = document.querySelector('.note-container')
+
+
+class Note {
+    constructor(title,content){
+        this.title = title
+        this.content = content
+    }
+
+     createNote() {
+        let note = document.createElement('div')
+        note.className = 'note'
+        let heading = document.createElement('h2')
+        heading.className = 'note-heading'
+        heading.innerText = this.title
+        let noteText = document.createElement('div')
+        noteText.className = 'content'
+        noteText.innerText = this.content
+        note.append(heading)
+        note.append(noteText)
+        return note;
+    }
+
+    pasteNote(_parent){
+        _parent.append(this.createNote())
+    }
+
+    deleteNote(){
+
+    }
+}
+
+let note1 = new Note('hello world','also some extra stuff with hello world')
+createNoteBtn.addEventListener('click',()=>{
+   let noteTitle = document.getElementById('note-title-box').value;
+   let noteContent = document.getElementById('note-content-box').value;
+   let note2 = new Note(noteTitle,noteContent);
+   note2.createNote();
+   console.log(parent)
+   note2.pasteNote(parent)
+})
