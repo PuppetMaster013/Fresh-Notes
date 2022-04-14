e7e5adfd1bac14fd83be120860e0f745fe56b4ab
aditya <adityakumar222333@gmail.com>
2022-04-14 17:03:54 +0530

    finally no errors but the code needs some refactoring

--- JS/noteManager.js
+++ JS/noteManager.js
@@ -1,6 +1,7 @@
 import note from "./notes.js";
 import local from "./storeLocal.js";
 import container from "./container.js";
+import utility from "./utility.js";
 
 export default class noteManager {
 
@@ -18,7 +19,7 @@ export default class noteManager {
   initialize() {
     if(noteManager.noteObjArr.length > 0) {
     noteManager.noteObjArr.forEach((noteObj) => {
-    this.noteContainer.placeOnDom(noteObj.createElement(this.archiveEvent));      
+    this.noteContainer.placeOnDom(this.createElement(this.archiveEvent,noteObj));      
     });
   }
   }
@@ -27,10 +28,23 @@ export default class noteManager {
         let noteTitle = document.querySelector("#note-title-box").value;
         let noteContent = document.querySelector("#note-content-box").value;
         let addedNote = new note(noteTitle, noteContent);
-        this.noteContainer.placeOnDom(addedNote.createElement(this.archiveEvent));
+        this.noteContainer.placeOnDom(this.createElement(this.archiveEvent,addedNote));
         noteManager.noteObjArr.push(addedNote)
         local.addLocal(addedNote);
       })
+  }
+  createElement(func,objNote) {
+    // utility.addElement()
+    let element = utility.addElement("div", "note", "");
+    element.setAttribute("id", objNote.myId);
+    let crossBtn = utility.addElement("button", "crossBtn", "⛔");
+    let noteHeading = utility.addElement("h2", "note-heading", objNote.title);
+    let noteContent = utility.addElement("div", "content", objNote.content);
+    crossBtn.addEventListener("click", func);
+    element.append(crossBtn);
+    element.append(noteHeading);
+    element.append(noteContent);
+    return element;
   }
   archiveEvent(e){
     noteManager.noteObjArr.forEach((noteObj) => {
--- JS/notes.js
+++ JS/notes.js
@@ -1,4 +1,4 @@
-import utility from "./utility.js";
+
 
 export default class Note {
   static count = 0;
@@ -11,17 +11,5 @@ export default class Note {
     this.state = "active";
   }
 
-  createElement(func) {
-    // utility.addElement()
-    let element = utility.addElement("div", "note", "");
-    element.setAttribute("id", this.myId);
-    let crossBtn = utility.addElement("button", "crossBtn", "⛔");
-    let noteHeading = utility.addElement("h2", "note-heading", this.title);
-    let noteContent = utility.addElement("div", "content", this.content);
-    crossBtn.addEventListener("click", func);
-    element.append(crossBtn);
-    element.append(noteHeading);
-    element.append(noteContent);
-    return element;
-  }
+  
 }
